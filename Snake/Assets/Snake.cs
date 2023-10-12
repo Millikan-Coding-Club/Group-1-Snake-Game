5b75a92cd5f50ed2615a056197354072fc4e0f91
YeetmyFeet <yeetmyfeet69420bruh@gmail.com>
2023-10-11 22:52:13 -0700

    Added Growing
    -updated Snake script
    -added snake segment prefab
    -decreased scale of snake to 0.9 to show segments

--- Snake/Assets/Snake.cs
+++ Snake/Assets/Snake.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
 
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W)) {
@@ -19,10 +27,31 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f
         );
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
+
     }
 }
